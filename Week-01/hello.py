c9ccb29a24731b33cbf7e56f8dcb78f113438b30
Bharath Reddy <pbreddy014@gmail.com>
2026-09-25 16:41:25 +0530

    Day 1 - Python setup and first script

---
+++ Week-01/hello.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+print("I am learning Python.")
+print("My goal is to build enterprise AI applications.")
+
+name = input("What is your name? ")
+
+print(f"Hello {name.upper()}, welcome")
+
+
