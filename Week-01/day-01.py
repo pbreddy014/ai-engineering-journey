960b2dbd4287b7cd7b59134702a5b5d105679867
Bharath Reddy <pbreddy014@gmail.com>
2026-09-25 16:56:25 +0530

    Day 1 - AI engineering learning profile

---
+++ Week-01/day-01.py
@@ -0,0 +1,16 @@
+name = "Bharath"
+hours = 2
+weeks = 52
+total_hours = hours * 52 * 7
+
+print("====================================")
+print("MY AI ENGINERRING JOURNEY")
+print("====================================")
+
+print(f"Name: {name}")
+print("Current Skills: Power BI, Analytics, Web Development \n\nPython Level: Beginner \n\nLearning Goal: \nBuild Enterprise AI Applications\n")
+
+print(f"Daily Learning: {hours} hours")
+print(f"Duration: {weeks} weeks")
+
+print(f"Total planned learning hours: {total_hours} ")
